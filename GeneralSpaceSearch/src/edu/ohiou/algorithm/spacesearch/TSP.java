d2baa98fc9bc575dae7df62f98aae51d48d64bc0
Nur Shomik <arafatnur01@gmail.com>
2016-09-22 16:22:15 -0400

    converted the project into a Maven project

--- GeneralSpaceSearch/src/edu/ohiou/algorithm/spacesearch/TSP.java
+++ GeneralSpaceSearch/src/edu/ohiou/algorithm/spacesearch/TSP.java
@@ -1,13 +1,44 @@
 package edu.ohiou.algorithm.spacesearch;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TSP extends State {
+	
+	private static final Map<String, Integer> cityList;
+	static{
+		Map<String, Integer> cities = new HashMap<String, Integer>();
+		cities.put("Athens", 0);
+		cities.put("Columbus", 1);
+		cities.put("Cincinnati", 2);
+		cities.put("Cleveland", 3);
+		cities.put("Marietta", 4);
+		cityList = Collections.unmodifiableMap(cities);
+	}
+	private static final int[][] distanceMatrix = { { 0, 75, 155, 206, 46 }, { 75, 0, 105, 137, 120 },
+			{ 155, 105, 0, 241, 225 }, { 206, 137, 241, 0, 160 }, { 46, 120, 225, 160, 0 } };
+	
+	private ArrayList<String> cities = null; //this is the "State value" of this state
+	private String firstCity = null, lastCity = null;
 
 	public TSP() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public TSP(String state) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TSP(TSP State) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TSP(int abc) {
+		// TODO Auto-generated constructor stub
+	}
+	
 	@Override
 	public boolean equals(Object st) {
 		// TODO Auto-generated method stub
